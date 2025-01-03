eac22c4011265709c8f3822a5737988add27b115
Chetan <chetanrjj2002@gmail.com>
2025-01-04 03:34:40 +0530

    created invite menu

--- client/src/components/inviteMenu.jsx
+++ client/src/components/inviteMenu.jsx
@@ -2,8 +2,44 @@
 
 export default function InviteMenu(){
     return (
-        <div className="invite-menu-container">
-            
+        <div className="invite-menu-bg">
+            <div className="invite-menu-container">
+                <div>
+                    <div className="invite-menu-titles">
+                        <img src="/squad_logo.svg" alt="" />
+                        <div>Squad</div>
+                    </div>
+
+                    <div className="invite-button">
+                        <img src="/squad_img.png" alt="" />
+                        <div>Squad Join</div>
+                    </div>
+                </div>
+
+                <div>
+                    <div className="invite-menu-titles">
+                        <img src="/online_logo.svg" alt="" />
+                        <div>Online</div>
+                    </div>
+
+                    <div className="invite-button">
+                        <img src="/online_img.png" alt="" />
+                        <div>Mary Jane</div>
+                    </div>
+                </div>
+
+                <div>
+                    <div className="invite-menu-titles">
+                        <img src="/offline_logo.svg" alt="" />
+                        <div>Offline</div>
+                    </div>
+
+                    <div className="invite-button">
+                        <img src="/offline_img.png" alt="" />
+                        <div>420</div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
