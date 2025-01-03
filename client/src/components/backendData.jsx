4d25c58f4661f8df5963ca09a9c1c508f4d21872
Chetan <chetanrjj2002@gmail.com>
2025-01-03 20:36:21 +0530

    changed api calls to debug the deployment error

--- client/src/components/backendData.jsx
+++ client/src/components/backendData.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react"
 export default function BackendData(){
 
     const [serverData, setServerData] = useState(null)
-    const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api/server-info";
+    const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api/server-info";
     useEffect(()=>{
         fetch(`${API_URL}`)
         .then(res=>res.json())
