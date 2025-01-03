e254682e6809288d2a921c50609eaed375d05ad7
Chetan <chetanrjj2002@gmail.com>
2025-01-03 17:21:33 +0530

    added the mock data and created an api endpoint

--- server/index.js
+++ server/index.js
@@ -7,8 +7,45 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Server is running!');
+const serverInfo = {
+  players: "60/64",
+  ping: "104ms",
+  tickrate: "60 Hz",
+  settings: {
+    region: "Europe - DE",
+    punkBuster: "ON",
+    fairFight: "ON",
+    password: "OFF",
+    preset: "NORMAL",
+  },
+  advanced: {
+    minimap: "ON",
+    onlySquadLeaderSpawn: "OFF",
+    vehicles: "ON",
+    teamBalance: "ON",
+    minimapSpotting: "ON",
+    hud: "ON",
+    "3pVehicleCam": "ON",
+    regenerativeHealth: "ON",
+    killCam: "ON",
+    friendlyFire: "OFF",
+    "3dSpotting": "ON",
+    enemyNameTags: "ON",
+  },
+  rules: {
+    tickets: 400,
+    vehicleSpawnDelay: 25,
+    bulletDamage: 100,
+    kickAfterTeamKills: 5,
+    playerHealth: 100,
+    playerRespawnTime: 100,
+    kickAfterIdle: 300,
+    banAfterKicks: 3,
+  },
+};
+
+app.get('/api/server-info', (req, res) => {
+  res.json(serverInfo);
 });
 
 app.listen(PORT, () => {
