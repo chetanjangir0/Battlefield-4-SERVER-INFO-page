b9f4fb83a863e4887957d703edffdd72d76bf8e6
Chetan <chetanrjj2002@gmail.com>
2025-01-03 11:21:13 +0530

    added breadcrumb menu

--- client/src/components/browserBreadcrumb.jsx
+++ client/src/components/browserBreadcrumb.jsx
@@ -1,21 +1,18 @@
 
-
-
-
-
 export default function BrowserBreadcrumb() {
     return (
         <div>
-            <div class="previous-breadcrumb">
-                <a href="#">
+            <div className="previous-breadcrumb">
+                <a href="#" className="breadcrumb-back">
                     <img src="https://cdn.prod.website-files.com/6013fff62154adaa4600f932/601ab21dde67c22de2b6d61b_back.svg" alt="" />
                 </a>
                 <a href="#">multiplayer</a>
-                <div class="breadcrumb-bar">/</div>
+                <div>/</div>
                 <a href="#">server Browser</a>
-                <div class="breadcrumb-bar">/</div>
-            </div>
+                <div>/</div>
 
+            </div>
+            
             <div className="current-breadcrumb">Server Info</div>
         </div>
     )
